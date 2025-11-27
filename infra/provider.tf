6f9116751f47d0b2b4148ca5764b91e2e11c9da8
KristijanBoshev <154442308+KristijanBoshev@users.noreply.github.com>
2025-11-27 15:11:14 +0100

    chore: remove provider versions

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,14 +3,5 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {}
 }
